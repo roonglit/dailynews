2845469190c4aff3adf4d9127b02a50ff3ac9e4e
Roonglit Chareonsupkul <roonglit@gmail.com>
2025-10-07 07:30:18 +0700

    remove policy temporary

--- app/policies/book_policy.rb
+++ app/policies/book_policy.rb
@@ -1,6 +1,7 @@
 class BookPolicy < ApplicationPolicy
   def show?
-    user.memberships.where("start_date <= ? and end_date >= ?", Date.today, Date.today).present?
+    # user.memberships.where("start_date <= ? and end_date >= ?", Date.today, Date.today).present?
+    true
   end
 
   class Scope < ApplicationPolicy::Scope
