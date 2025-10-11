7b97bc937038fd4be796457a6ba0dd2e8eb201b1
Roonglit Chareonsupkul <roonglit@gmail.com>
2025-10-11 12:20:48 +0700

    add checkout page and prepare for payment

---
+++ app/controllers/checkouts_controller.rb
@@ -0,0 +1,7 @@
+class CheckoutsController < ApplicationController
+  def show
+  end
+
+  def create
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,15 @@
 Rails.application.routes.draw do
+  devise_for :users, controllers: { registrations: "users/registrations" }
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
   resources :memberships
   resources :books
+  resource :checkout, only: %i[show create]
 
   namespace :admin do
     resources :books
     root "home#index"
   end
-
-  devise_for :users, controllers: { registrations: "users/registrations" }
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
