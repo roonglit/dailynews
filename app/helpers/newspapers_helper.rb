26dc4e5b2ff95e842a1bf4b55b4943332bd5e04d
danudeth2004 <oat25482547@gmail.com>
2025-10-15 16:58:29 +0700

    fix: change conditionf for check memberships of newspaper

--- app/helpers/newspapers_helper.rb
+++ app/helpers/newspapers_helper.rb
@@ -1,5 +1,5 @@
 module NewspapersHelper
   def check_newspaper_memberships(newspaper, user)
-    user&.memberships&.where("start_date >= ? and end_date >= ?", newspaper.published_at.to_date, Date.today).present?
+    user&.memberships&.where("start_date <= ? and end_date >= ?", newspaper&.published_at&.to_date || newspaper.created_at.to_date, Date.today).present?
   end
 end
