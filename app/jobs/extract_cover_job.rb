dfeb22c536bc7c8bc27f91a17b48a84f51e2fb25
Roonglit <roonglit@gmail.com>
2025-10-15 10:25:47 +0700

    fix extract cover

--- app/jobs/extract_cover_job.rb
+++ app/jobs/extract_cover_job.rb
@@ -1,14 +1,14 @@
 class ExtractCoverJob < ApplicationJob
   queue_as :default
 
-  def perform(book_id)
-    book = Book.find(book_id)
+  def perform(newspaper_id)
+    newspaper = Newspaper.find(newspaper_id)
 
     # Check if PDF is attached
-    return unless book.pdf.attached?
+    return unless newspaper.pdf.attached?
 
     # load pdf file
-    book.pdf.open do |pdf_file|
+    newspaper.pdf.open do |pdf_file|
       # Create a temporary file for the cover image
       cover_tempfile = Tempfile.new([ "cover", ".jpg" ])
 
@@ -17,10 +17,10 @@ class ExtractCoverJob < ApplicationJob
         images = PDFToImage.open(pdf_file.path)
         images[0].r("72").resize("40%").save(cover_tempfile.path)
 
-        # Attach the cover image to the book
-        book.cover.attach(
+        # Attach the cover image to the newspaper
+        newspaper.cover.attach(
           io: File.open(cover_tempfile.path),
-          filename: "#{book.id}_cover.jpg",
+          filename: "#{newspaper.id}_cover.jpg",
           content_type: "image/jpeg"
         )
       ensure
