86b46472492862a1b5b57a40530cbfc595d2828d
yotsaphat <yotsaphat.lert@odds.team>
2025-10-15 17:22:25 +0700

    fix: handle blank membership

--- app/controllers/libraries_controller.rb
+++ app/controllers/libraries_controller.rb
@@ -1,6 +1,13 @@
 class LibrariesController < ApplicationController
+  before_action :authenticate_user!, only: %i[ show ]
+
   def show
     @newspapers = []
+
+    if current_user.memberships.blank?
+      redirect_to root_path
+      return
+    end
 
     current_user.memberships.each do |membership|
       newspapers = Newspaper.where("published_at >= ? and published_at <= ?", membership.start_date, membership.end_date).order_by_created_at
@@ -8,6 +15,5 @@ class LibrariesController < ApplicationController
     end
 
     @newspapers = @newspapers.uniq
-
   end
 end
