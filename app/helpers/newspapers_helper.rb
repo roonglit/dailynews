47d0423591856500858fa399626d62bd47aa6be5
danudeth2004 <oat25482547@gmail.com>
2025-10-15 18:01:15 +0700

    feat: condition for check memberships of newspaper

--- app/helpers/newspapers_helper.rb
+++ app/helpers/newspapers_helper.rb
@@ -1,5 +1,12 @@
 module NewspapersHelper
   def check_newspaper_memberships(newspaper, user)
-    user&.memberships&.where("start_date <= ? and end_date >= ?", newspaper&.published_at&.to_date || newspaper.created_at.to_date, Date.today).present?
+    # user&.memberships&.where("start_date <= ? and end_date >= ?", newspaper&.published_at&.to_date || newspaper.created_at.to_date, Date.today).present?
+
+    user.memberships.each do |membership|
+      if newspaper.published_at >= membership.start_date && newspaper.published_at <= membership.end_date
+        return true
+      end
+    end
+    false
   end
 end
