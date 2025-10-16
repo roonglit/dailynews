986e05858838a914fb95781fc00f9ce4dc961a99
danudeth2004 <oat25482547@gmail.com>
2025-10-16 10:19:40 +0700

    test: can visit to newspaper details page

--- spec/factories/newspapers.rb
+++ spec/factories/newspapers.rb
@@ -1,6 +1,7 @@
 FactoryBot.define do
   factory :newspaper do
     title { "Example Book" }
+    description {  "Description example" }
     published_at { Date.today }
     created_at { Date.today }
   end
--- spec/system/newspaper_details_spec.rb
+++ spec/system/newspaper_details_spec.rb
@@ -1,6 +1,12 @@
 require 'rails_helper'
 
 describe "Newspaper Details" do
-  it "" do
+  let!(:newspaper) { create(:newspaper) }
+
+  it "allows a visitor to view a newspaper's details" do
+    visit newspaper_path(newspaper)
+
+    expect(page).to have_content("Example Book")
+    expect(page).to have_content("Description example")
   end
 end
