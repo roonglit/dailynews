5f96ad00aebb6b408b3950e29b1c5730c800c9bf
yotsaphat <yotsaphat.lert@odds.team>
2025-10-16 16:24:00 +0700

    add system test library spec

---
+++ spec/system/library_spec.rb
@@ -0,0 +1,38 @@
+require 'rails_helper'
+
+describe "Library", js: true do
+  let!(:user) { create(:member) }
+
+  before { login_as_user(user) }
+  it "if the user is not logged in, the library will be empty." do
+    visit library_path
+
+    expect(page).to have_content("No newspapers available")
+    expect(page).to have_content("Check back later for new additions to our library.")
+  end
+
+  context "there are newspapers but no membership" do
+    let!(:newspaper) { create(:newspaper) }
+
+    it "there is no membership, the library will be empty" do
+      visit library_path
+
+      expect(page).to have_content("No newspapers available")
+      expect(page).to have_content("Check back later for new additions to our library.")
+    end
+  end
+
+  context "there are newspapers and membership" do
+    let!(:newspaper) { create(:newspaper) }
+    before { create(:membership, user: user) }
+
+    it "the library will be visible" do
+      visit library_path
+
+      expect(page).to have_content("Example Book")
+      expect(page).to have_content("Description example")
+      expect(page).not_to have_content("No newspapers available")
+      expect(page).not_to have_content("Check back later for new additions to our library.")
+    end
+  end
+end
