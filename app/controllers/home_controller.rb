8ff3353961ba4bc5c01aff9c3b645939a77528a8
fingernew@odds.team <fingernew@odds.team>
2025-10-17 16:13:38 +0700

    fix: add navbar and fix style home page.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,7 @@
 class HomeController < ApplicationController
   def index
     @newspapers = Newspaper.all
+    @monthly_product = current_user.cart&.cart_item&.product
+    @latest_newspaper = Newspaper.where.not(published_at: nil).order(published_at: :desc).first
   end
 end
