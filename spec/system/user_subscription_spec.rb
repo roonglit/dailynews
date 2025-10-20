179b23dcb3677f02c3e211c48b2ee81969bda16b
danudeth2004 <oat25482547@gmail.com>
2025-10-20 13:17:29 +0700

    test: fix system follow website flow

--- spec/system/user_subscription_spec.rb
+++ spec/system/user_subscription_spec.rb
@@ -3,20 +3,11 @@ require 'rails_helper'
 describe "User interactions with newspaper subscriptions", js: true do
   context "when user is a guest" do
     let(:newspaper) { create(:newspaper) }
-    let!(:one_month_product) { create(:one_month) }
     let!(:subscribe_monthly_product) { create(:subscribe_monthly) }
 
-    it "allows guest to add product to cart and view checkout page" do
-      visit newspaper_path(newspaper)
-
-      click_button "1 Month Only"
-
-      expect(page).to have_content("Product added to cart")
-    end
-
     it "display authentication modal when guest clicks continuing to payment" do
-      visit newspaper_path(newspaper)
-      click_button "1 Month Only"
+      visit root_path
+      click_button "สมัครสมาชิก"
 
       click_link_or_button "Continue to Payment"
 
@@ -26,22 +17,20 @@ describe "User interactions with newspaper subscriptions", js: true do
 
   context "when user is a member" do
     let(:newspaper) { create(:newspaper) }
-    let!(:one_month_product) { create(:one_month) }
     let!(:subscribe_monthly_product) { create(:subscribe_monthly) }
 
     before { login_as_user }
 
     it "allows member to add product and redirects to checkout" do
-      visit newspaper_path(newspaper)
-
-      click_button "Subscribe Monthly"
+      visit root_path
+      click_button "สมัครสมาชิก"
 
       expect(page).to have_content("Product added to cart")
     end
 
     # it "display omise modal when guest clicks continuing to payment" do
-    #   visit newspaper_path(newspaper)
-    #   click_button "1 Month Only"
+    #   visit root_path
+    #   click_button "สมัครสมาชิก"
 
     #   click_link_or_button "Continue to Payment"
 
@@ -49,8 +38,8 @@ describe "User interactions with newspaper subscriptions", js: true do
     # end
 
     it "[Test] allows member to grants a one-month membership when member clicks free subscription" do
-      visit newspaper_path(newspaper)
-      click_button "Subscribe Monthly"
+      visit root_path
+      click_button "สมัครสมาชิก"
 
       accept_confirm "Skip payment and grant free membership?" do
         click_button "Grant Free Membership (Testing)"
