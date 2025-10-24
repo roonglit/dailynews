504cd0f4874377a72a18253823e7f938263e0b4d
danudeth2004 <oat25482547@gmail.com>
2025-10-24 16:48:52 +0700

    feat: update customer information

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -14,12 +14,26 @@ module Admin
     end
 
     def update
+      respond_to do |format|
+        if @member.update(member_params)
+          ExtractCoverJob.perform_later @member.id
+          format.html { redirect_to admin_customer_path(@member), notice: "Member was successfully updated.", status: :see_other }
+          format.json { render :show, status: :ok, location: @member }
+        else
+          format.html { render :edit, status: :unprocessable_entity }
+          format.json { render json: @member.errors, status: :unprocessable_entity }
+        end
+      end
     end
 
   private
 
     def set_member
       @member = Member.find(params.expect(:id))
+    end
+
+    def member_params
+      params.expect(member: [ :id, :first_name, :last_name ])
     end
   end
 end
