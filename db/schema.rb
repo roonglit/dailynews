ae4768062a9210b07f98eae617dee20e36af40b9
danudeth2004 <oat25482547@gmail.com>
2025-10-24 15:32:35 +0700

    feat: add name to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2025_10_23_151110) do
+ActiveRecord::Schema[8.1].define(version: 2025_10_24_082733) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -60,8 +60,6 @@ ActiveRecord::Schema[8.1].define(version: 2025_10_23_151110) do
     t.datetime "reset_password_sent_at"
     t.string "reset_password_token"
     t.datetime "updated_at", null: false
-    t.index ["email"], name: "index_admin_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_admin_users_on_reset_password_token", unique: true
   end
 
   create_table "cart_items", force: :cascade do |t|
@@ -137,6 +135,8 @@ ActiveRecord::Schema[8.1].define(version: 2025_10_23_151110) do
     t.datetime "created_at", null: false
     t.string "email"
     t.string "encrypted_password"
+    t.string "first_name"
+    t.string "last_name"
     t.datetime "remember_created_at"
     t.datetime "reset_password_sent_at"
     t.string "reset_password_token"
