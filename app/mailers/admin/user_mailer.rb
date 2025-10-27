2239d94d34f5a4a42737db737b927665ded3fc7f
danudeth2004 <oat25482547@gmail.com>
2025-10-27 16:51:59 +0700

    feat: test send email

---
+++ app/mailers/admin/user_mailer.rb
@@ -0,0 +1,10 @@
+module Admin
+  class UserMailer < ActionMailer::Base
+    default from: Rails.application.credentials.dig(:smtp, :user_name)
+
+    def invite_admin(email)
+      @email = email
+      mail(to: email, subject: "Invite admin, u can init your password")
+    end
+  end
+end
---
+++ spec/mailers/previews/admin/user_mailer_preview.rb
@@ -0,0 +1,4 @@
+# Preview all emails at http://localhost:3000/rails/mailers/admin/user_mailer_mailer
+class Admin::UserMailerPreview < ActionMailer::Preview
+
+end
