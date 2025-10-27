8ef0b6b5639548099afc518cb2d4b6307f2eaea7
yotsaphat <yotsaphat.lert@odds.team>
2025-10-27 15:30:31 +0700

    correct admin count

--- app/controllers/admin/teams_controller.rb
+++ app/controllers/admin/teams_controller.rb
@@ -6,8 +6,8 @@ module Admin
       items_per_page = params[:per_page].to_i
       items_per_page = 10 unless items_per_page.positive?
       page = params[:page].present? && params[:page].to_i > 0 ? params[:page].to_i : 1
-      admins = Admin::User.search(params[:q]).order(:id)
-      @pagy, @admins = pagy(admins, limit: items_per_page, page: page, params: { q: params[:q], per_page: params[:per_page] }.compact)
+      @all_admins = Admin::User.search(params[:q]).order(:id)
+      @pagy, @admins = pagy(@all_admins, limit: items_per_page, page: page, params: { q: params[:q], per_page: params[:per_page] }.compact)
     end
 
     def new
