a849b61adeecf07dd8a4f46b6998adf237115fa5
yotsaphat <yotsaphat.lert@odds.team>
2025-10-28 15:40:21 +0700

    add system test prefill email case

--- spec/system/admin/first_user_can_create_admin_user_spec.rb
+++ spec/system/admin/first_user_can_create_admin_user_spec.rb
@@ -30,4 +30,13 @@ describe "Create Admin user" do
       expect(page).to have_content("Signed in successfully.")
     end
   end
+
+  context "when visiting with email param" do
+    it "prefills and disables the email field" do
+      visit "/admin/first_users/new?mail=abc@hotmail.com"
+
+      expect(page).to have_content("Admin Sign Up")
+      expect(page).to have_field("email", with: "abc@hotmail.com", disabled: true)
+    end
+  end
 end
