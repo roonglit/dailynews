b2c6ce20bbeb090b649281c7f4bdf19d4807564c
yotsaphat <yotsaphat.lert@odds.team>
2025-10-29 15:07:52 +0700

    add system test for invite admin teams flow

--- spec/system/admin/admin_manager_spec.rb
+++ spec/system/admin/admin_manager_spec.rb
@@ -1,13 +1,73 @@
 require 'rails_helper'
 
-describe "Manage Admin user" do
-  context "when admin click subscription button" do
-    it "admin user can access member subscription" do
-      login_as_admin
+RSpec.configure do |config|
+  config.before(:each) do
+    FactoryBot.rewind_sequences
+  end
 
-      click_link_or_button "Subscriptions"
+  describe "Manage Admin user" do
+    context "when admin click Admin Teams button" do
+      before { @admin_users = create_list(:admin_user, 5) }
 
-      expect(page).to have_content("Subscriptions")
+      it "admin user can view all Admin Teams" do
+        login_as_admin
+
+        expect(page).to have_link("Admin Teams")
+        click_link_or_button "Admin Teams"
+
+        expect(page).to have_current_path(admin_teams_path)
+        expect(page).to have_content("6 Team Admins")
+        expect(page).to have_content("admin1@example.com")
+      end
+
+      it "admin user can access member Invite Admin Teams page" do
+        login_as_admin
+
+        click_link_or_button "Admin Teams"
+        click_link_or_button "Invite Team Members"
+
+        expect(page).to have_current_path(new_admin_team_path)
+        expect(page).to have_content("Invite Team Members")
+        expect(page).to have_content("Enter the email addresses of people you want to invite.")
+      end
+
+      it "when admin press back from Invite Admin Teams should navigate to Admin Teams page" do
+        login_as_admin
+
+        click_link_or_button "Admin Teams"
+        click_link_or_button "Invite Team Members"
+        click_link_or_button "Back"
+
+        expect(page).to have_current_path(admin_teams_path)
+        expect(page).to have_content("6 Team Admins")
+        expect(page).to have_content("admin1@example.com")
+      end
+
+      it "when admin press sent invite button with empty email should show error message box" do
+        login_as_admin
+
+        click_link_or_button "Admin Teams"
+        click_link_or_button "Invite Team Members"
+        click_link_or_button "Send Invite"
+
+        expect(page).to have_current_path(new_admin_team_path)
+        expect(page).to have_content("Email can't be blank")
+        expect(page).not_to have_content("Invitation sent successfully.")
+      end
+
+      it "when admin press sent invite button with valid email should show success message box" do
+        login_as_admin
+
+        click_link_or_button "Admin Teams"
+        click_link_or_button "Invite Team Members"
+
+        fill_in 'invite_email', with: "admin10@hotmail.com"
+        click_link_or_button "Send Invite"
+
+        expect(page).to have_current_path(new_admin_team_path)
+        expect(page).not_to have_content("Email can't be blank")
+        expect(page).to have_content("Invitation sent successfully.")
+      end
     end
   end
 end
