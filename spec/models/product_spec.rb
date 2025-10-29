3d3be69a7b0446ebc110ddf25155dc0ac904b9b1
danudeth2004 <oat25482547@gmail.com>
2025-10-29 13:46:33 +0700

    test: product model

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,34 +1,31 @@
 require 'rails_helper'
 
 RSpec.describe Product, type: :model do
-  let!(:one_month) { create(:one_month) }
+  let!(:one_month_product) { create(:one_month_product) }
+  let(:member) { create(:member) }
+  let(:product) { create(:monthly_subscription_product, amount: nil) }
+  TAX_RATE = 0.07
 
   it "not create then title is nil" do
     product = build(:product, title: nil)
     expect(product).not_to be_valid
-    expect(product.errors[:title]).to include("can't be blank")
-  end
-
-  it "not create then amount is nil" do
-    product = build(:product, amount: nil)
-    expect(product).not_to be_valid
-    expect(product.errors[:amount]).to include("can't be blank")
+    expect(product.errors[:title]).to include("Can't be blank")
   end
 
   it "not create then sku is nil" do
     product = build(:product, sku: nil)
     expect(product).not_to be_valid
-    expect(product.errors[:sku]).to include("can't be blank")
+    expect(product.errors[:sku]).to include("Can't be blank")
   end
 
   it "is invalid with a duplicate title" do
-    product = build(:product, title: one_month.title)
+    product = build(:product, title: one_month_product.title)
     expect(product).not_to be_valid
     expect(product.errors[:title]).to include("has already been taken")
   end
 
   it "is invalid with a duplicate sku" do
-    product = build(:product, sku: one_month.sku)
+    product = build(:product, sku: one_month_product.sku)
     expect(product).not_to be_valid
     expect(product.errors[:sku]).to include("has already been taken")
   end
@@ -36,5 +33,32 @@ RSpec.describe Product, type: :model do
   it "is valid with all required attributes" do
     product = build(:product, title: "New Product", amount: 100, sku: "NEW_SKU")
     expect(product).to be_valid
+  end
+
+  context "product price exists" do
+    let(:product_have_amount) { create(:product) }
+
+    it "should be base price equal than product price / 1.07" do
+      cal = product_have_amount.amount.cents / 1.07
+      expect(product_have_amount.base_price).to eq(cal.round)
+    end
+
+    it "should be tax amount equal than amount - base price" do
+      expect(product_have_amount.tax_amount).to eq(product_have_amount.amount.cents - product_have_amount.base_price)
+    end
+  end
+
+  context "product price does not exists" do
+    it "should be base price equal than 0 when not have product price" do
+      expect(product.base_price).to eq(0)
+    end
+
+    it "should be tax amount equal than 0 when not have product price" do
+      expect(product.tax_amount).to eq(0)
+    end
+  end
+
+  it "should be return tax rate percentage" do
+    expect(product.tax_rate_percentage).to eq((TAX_RATE * 100).to_i)
   end
 end
