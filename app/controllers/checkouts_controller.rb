aef9fbd3cec97d26771b8e50b2949799aea127f1
fingernew@odds.team <fingernew@odds.team>
2025-10-30 09:29:01 +0700

    fix: remove "session[:member_return_to] = request.fullpath."

--- app/controllers/checkouts_controller.rb
+++ app/controllers/checkouts_controller.rb
@@ -1,23 +1,17 @@
 class CheckoutsController < ApplicationController
   before_action :redirect_if_has_active_subscription
-  before_action :check_member_sign_in?
   before_action :required_cart_item
 
   def show
+    if member_signed_in?
+      @order = Order.new(member: current_user)
+    end
     @cart = current_user&.cart
     @cart_item = @cart&.cart_item
     @product = @cart_item&.product
   end
 
   private
-
-  def check_member_sign_in?
-    if member_signed_in?
-      @order = Order.new(member: current_user)
-    else
-      session[:member_return_to] = request.fullpath
-    end
-  end
 
   def required_cart_item
     redirect_to root_path if current_user.cart&.cart_item.blank?
