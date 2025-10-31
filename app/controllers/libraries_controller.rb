3af3bbf03a522a5bd46705b052981fe2d9a0dff6
danudeth2004 <oat25482547@gmail.com>
2025-10-31 15:24:25 +0700

    feat: dont show member subscription when subscription is nil

--- app/controllers/libraries_controller.rb
+++ app/controllers/libraries_controller.rb
@@ -8,7 +8,10 @@ class LibrariesController < ApplicationController
       (subscription.start_date..subscription.end_date).map { |day| day.strftime("%m") }
     end.flatten.uniq.sort
 
-    conditions = current_user.subscriptions.map do |subscription|
+    member_subscriptions = current_user.subscriptions
+    return if member_subscriptions.blank?
+
+    conditions = member_subscriptions.map do |subscription|
       "(published_at >= '#{subscription.start_date}' and published_at <= '#{subscription.end_date}')"
     end
 
