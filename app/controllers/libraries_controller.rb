abfd8c99a17676b764a8b7456c8297741baf984d
danudeth2004 <oat25482547@gmail.com>
2025-10-31 15:42:02 +0700

    feat: member dont access to library when not have subscription

--- app/controllers/libraries_controller.rb
+++ app/controllers/libraries_controller.rb
@@ -1,5 +1,6 @@
 class LibrariesController < ApplicationController
   before_action :authenticate_member!, only: %i[ show ]
+  before_action :required_subscription
 
   def show
     @newspapers = []
@@ -8,13 +9,14 @@ class LibrariesController < ApplicationController
       (subscription.start_date..subscription.end_date).map { |day| day.strftime("%m") }
     end.flatten.uniq.sort
 
-    member_subscriptions = current_user.subscriptions
-    return if member_subscriptions.blank?
-
-    conditions = member_subscriptions.map do |subscription|
+    conditions = current_user.subscriptions.map do |subscription|
       "(published_at >= '#{subscription.start_date}' and published_at <= '#{subscription.end_date}')"
     end
 
     @newspapers = Newspaper.where(conditions).filter_by_month(params[:month]).order_by_created_at.distinct
+  end
+
+  def required_subscription
+    redirect_to root_path, alert: t("alerts.messages.require_subscription") if current_user.subscriptions.blank?
   end
 end
