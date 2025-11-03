48d1826c209094b468dfae5d646e5b84d1f1907e
yotsaphat <yotsaphat.lert@odds.team>
2025-11-03 11:20:47 +0700

    fix customer spec

--- spec/requests/admin/customers_spec.rb
+++ spec/requests/admin/customers_spec.rb
@@ -1,30 +1,48 @@
 require 'rails_helper'
 
 RSpec.describe "Admin::Customers", type: :request do
+  let!(:member) { create(:member, first_name: "firstname", last_name: "lastname") }
+  let!(:admin) { create(:admin_user) }
+  before { sign_in admin }
   describe "GET /index" do
     it "returns http success" do
-      get "/admin/customers/index"
+      get "/e-newspaper/admin/customers"
       expect(response).to have_http_status(:success)
     end
   end
 
   describe "GET /show" do
     it "returns http success" do
-      get "/admin/customers/show"
+      get "/e-newspaper/admin/customers/#{member.id}"
       expect(response).to have_http_status(:success)
     end
   end
 
   describe "GET /edit" do
     it "returns http success" do
-      get "/admin/customers/edit"
+      get "/e-newspaper/admin/customers/#{member.id}/edit"
       expect(response).to have_http_status(:success)
     end
   end
 
-  describe "GET /update" do
+  describe "PUT /update" do
+    it "updates and redirects" do
+      put "/e-newspaper/admin/customers/#{member.id}", params: {
+        member: {
+          id: member.id,
+          first_name: "Pom",
+          last_name: "Updated"
+        }
+      }
+      expect(response).to have_http_status(:redirect)
+      follow_redirect!
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "GET /subscriptions/new" do
     it "returns http success" do
-      get "/admin/customers/update"
+      get "/e-newspaper/admin/customers/#{member.id}/subscriptions/new"
       expect(response).to have_http_status(:success)
     end
   end
