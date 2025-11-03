e1c1f7792e94ce56cef42f824ec4666ab8915076
fingernew@odds.team <fingernew@odds.team>
2025-11-03 11:29:48 +0700

    fix test: account subscriptions.

--- spec/requests/subscriptions_spec.rb
+++ spec/requests/subscriptions_spec.rb
@@ -2,7 +2,7 @@ require "rails_helper"
 
 RSpec.describe "Subscriptions", type: :request do
   let(:member) { create(:member) }
-  let(:membership) { create(:membership, user: member, auto_renew: false) }
+  let(:subscription) { create(:subscription, member: member, auto_renew: false) }
 
   before do
     sign_in member  # Devise helper
@@ -10,25 +10,22 @@ RSpec.describe "Subscriptions", type: :request do
 
   describe "GET /subscriptions" do
     it "returns http success" do
-      get subscriptions_path
-      expect(response).to have_http_status(:ok)
-    end
+      get account_subscriptions_path
 
-    it "uses the account_setting layout (class-level check)" do
-      expect(SubscriptionsController._layout).to eq("account_setting")
+      expect(response).to have_http_status(:ok)
     end
   end
 
   describe "PATCH /subscriptions/:id" do
     it "updates auto_renew and redirects with notice" do
-      patch subscription_path(membership), params: { membership: { auto_renew: true } }
+      patch account_subscription_path(subscription), params: { subscription: { auto_renew: true } }
 
-      expect(response).to redirect_to(subscriptions_path)
+      expect(response).to redirect_to(account_subscriptions_path)
       follow_redirect!
       expect(response.body).to include("Auto renew updated successfully.")
 
-      membership.reload
-      expect(membership.auto_renew).to be true
+      subscription.reload
+      expect(subscription.auto_renew).to be true
     end
   end
 end
