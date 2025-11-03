a040043e9abca6cf826327a025c4c188f8af59b1
fingernew@odds.team <fingernew@odds.team>
2025-11-03 10:15:35 +0700

    fix test: admin subscriptions controller.

--- spec/requests/admin/subscriptions_spec.rb
+++ spec/requests/admin/subscriptions_spec.rb
@@ -1,9 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe "Admin::Subscriptions", type: :request do
-  describe "GET /show" do
+  let!(:admin) { create(:admin_user) }
+
+  before { sign_in admin }
+
+  describe "GET /index" do
     it "returns http success" do
-      get admin_subscription_path
+      get admin_subscriptions_path()
+
       expect(response).to have_http_status(:success)
     end
   end
