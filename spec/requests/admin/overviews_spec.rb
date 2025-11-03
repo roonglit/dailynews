62b4966756f0991da5332f1c8a489056cb381de3
yotsaphat <yotsaphat.lert@odds.team>
2025-11-03 11:22:41 +0700

    fix overview spec

--- spec/requests/admin/overviews_spec.rb
+++ spec/requests/admin/overviews_spec.rb
@@ -1,6 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe "Admin::Overviews", type: :request do
+  let!(:admin) { create(:admin_user) }
+  before { sign_in admin }
   describe "GET /show" do
     it "returns http success" do
       get admin_overview_path
