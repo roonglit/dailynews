a77689d48967d78aa51aca57dee451d987ed7856
danudeth2004 <oat25482547@gmail.com>
2025-11-03 11:26:33 +0700

    fix: path of first users request spec

--- spec/requests/admin/first_users_spec.rb
+++ spec/requests/admin/first_users_spec.rb
@@ -1,9 +1,9 @@
 require 'rails_helper'
 
-RSpec.describe "FirstAdmins", type: :request do
+RSpec.describe "Admin::FirstUsers", type: :request do
   describe "GET /new" do
     it "returns http success" do
-      get "/e-newspaper/admin/first_admins/new"
+      get "/e-newspaper/admin/first_users/new"
       expect(response).to have_http_status(:success)
     end
   end
