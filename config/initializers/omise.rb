fcab83a74f9d2f9861656ea1d5dab3efe5dabe2c
danudeth2004 <oat25482547@gmail.com>
2025-11-04 13:25:39 +0700

    fix: use mock api key for system test omise fake server

--- config/initializers/omise.rb
+++ config/initializers/omise.rb
@@ -1,3 +1,8 @@
 require "omise"
 
-Omise.api_key = Rails.application.credentials.dig(:omise, :secret_key)
+
+if Rails.env.test?
+  Omise.api_key = "test_secret_key"
+else
+  Omise.api_key = Rails.application.credentials.dig(:omise, :secret_key)
+end
