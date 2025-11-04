15ea32fe97d3382d0599e8bd6ac4e691d8365d32
Roonglit Chareonsupkul <roonglit@gmail.com>
2025-11-04 21:27:25 +0700

    update configuration for huawei

--- db/schema.rb
+++ db/schema.rb
@@ -78,6 +78,8 @@ ActiveRecord::Schema[8.1].define(version: 2025_11_03_023514) do
     t.string "reset_password_token"
     t.integer "status", default: 1, null: false
     t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_admin_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_admin_users_on_reset_password_token", unique: true
     t.index ["status"], name: "index_admin_users_on_status"
   end
 
