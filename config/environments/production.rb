fe4baf188e123e9f677ce3a4406d5138273a20b5
Roonglit Chareonsupkul <roonglit@gmail.com>
2025-11-04 21:42:31 +0700

    update configurations for app to have relative url

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -24,6 +24,9 @@ Rails.application.configure do
   # Store uploaded files on the local file system (see config/storage.yml for options).
   config.active_storage.service = :huaweicloud
 
+  # Set the relative URL root for apps behind load balancer with path prefix
+  config.relative_url_root = "/e-newspaper"
+
   # Assume all access to the app is happening through a SSL-terminating reverse proxy.
   config.assume_ssl = true
 
